b010a941f92cb4143267135744d2ccda3788a8c2
G\kornp <kornpitukgunk@gmail.com>
2025-05-14 18:08:12 +0700

    Feat: build monorepo

---
+++ version2/javascript-version/full-version/src/navigation/vertical/dashboardSRP.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: 'Dashboards',
+    icon: { icon: 'ri-radio-button-line' },
+    to: 'dashboards-crm',
+  },
+  {
+    title: 'Stock Update',
+    icon: { icon: 'ri-radio-button-line' },
+    to: 'apps-email',
+  },
+  {
+    title: 'Receiving',
+    icon: { icon: 'ri-radio-button-line' },
+    to: 'forms-autocomplete',
+  },
+  {
+    title: 'Request for Picking',
+    icon: { icon: 'ri-radio-button-line' },
+    to: 'forms-autocomplete',
+  },
+  {
+    title: 'Packing/PTW',
+    icon: { icon: 'ri-radio-button-line' },
+    to: 'forms-autocomplete',
+  },
+  {
+    title: 'Setting',
+    icon: { icon: 'ri-file-copy-line' },
+    children: [
+      {
+        title: 'Permission',
+        to: 'front-pages-landing-page',
+        target: '_blank',
+      },
+    ],
+  },
+]
---
+++ version2/javascript-version/full-version/src/pages/orgSRP/PackingPTW/index.vue
@@ -0,0 +1,12 @@
+<template lang="">
+  <div>
+    asdasdasd
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
